037485cbf91a24878800954fe1dcb35a177d86d2
Guilherme Barbosa <111584900+guilherme-barbosa-rodrigues@users.noreply.github.com>
2023-10-21 00:22:16 -0300

    completo

---
+++ enviar.js
@@ -0,0 +1,10 @@
+var enviar = document.getElementById('botao');
+
+function enviado(){
+    alert('Obrigado por enviar sua denúncia, conte sempre com as forças de segurança.')
+}
+
+enviar.onclick = function () {
+    enviado()
+}
+
